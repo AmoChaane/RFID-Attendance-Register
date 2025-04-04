c7e104f938408274e20532c03622fc9efe69af3a
AmoChaaneQualtrix <amogelang.chaane@qualtrix.co.za>
2025-04-04 04:43:06 +0200

    Able to use the Serial Monitor; Able to connect to Wi-Fi; Able to make an HTTP request but the problem is that we can't make a call to an HTTPS endpoint, it starts complaining about ssl.

---
+++ src/main.cpp
@@ -0,0 +1,81 @@
+#include <Arduino.h>
+#include "WiFi.h"
+#include <HTTPClient.h>
+
+const char* ssid = "VC-1012-9086";
+const char* password = "41a4843464";
+// String serverUrl = "https://e897-41-135-7-11.ngrok-free.app/api/v1/health/";
+const char* serverUrl = "https://e897-41-135-7-11.ngrok-free.app/api/v1/health/"; 
+
+// This is a forward declaration of a custom function named myFunction. 
+// Allows the compiler to recognize myFunction before its actual definition (later in the code)
+// This allows to call the function before it is declared
+bool connectToWiFi();
+void makeHttpGetRequest();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Serial Monitor Started!!!");
+  delay(1000);
+
+  if (!connectToWiFi()) {
+    Serial.println("Check credentials or hardware.");
+    while (1); // Halt if connection fails
+  }
+
+  makeHttpGetRequest();
+}
+
+void loop() {
+  // delay(1000);
+}
+
+bool connectToWiFi() {
+  //Configures the ESP32 to operate in Station (STA) mode (i.e., as a client that connects to a router).
+  // Disables unused modes (like Access Point mode) to save resources.
+  WiFi.mode(WIFI_STA); 
+  WiFi.begin(ssid, password);
+  
+  Serial.println("Connecting to WiFi...");
+  // millis() Gets the current time (in milliseconds) since the ESP32 started.
+  // Purpose: Stores the start time to enforce a 20-second timeout (prevents infinite hanging).
+  unsigned long startAttemptTime = millis();
+
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 20000) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("\nFailed to connect!");
+    return false;
+  }
+
+  Serial.println("\nConnected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  return true;
+}
+
+void makeHttpGetRequest() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverUrl);
+
+    int httpCode = http.GET();  // Send the GET request
+
+    if (httpCode > 0) {  // Check for a response
+      String payload = http.getString();  // Get the response payload
+      Serial.print("HTTP Response Code: ");
+      Serial.println(httpCode);
+      Serial.print("Response: ");
+      Serial.println(payload);
+    } else {
+      Serial.print("GET request failed. Error: ");
+      Serial.println(http.errorToString(httpCode).c_str());
+    }
+    http.end();  // Free resources
+  } else {
+    Serial.println("WiFi not connected!");
+  }
+}
